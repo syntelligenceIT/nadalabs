c95e0ef0a3e18004d9066764f23eefc9afa6ecc5
Miyuru Dharmage <voxsar@gmail.com>
2025-09-15 20:32:50 +0530

    data

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Poppins, Figtree, Inter } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+const figtree = Figtree({
+  variable: "--font-figtree",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Nada Labs",
+  description: "Simplifying Managed Services",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} ${figtree.variable} ${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
